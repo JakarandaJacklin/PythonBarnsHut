bd52336cc01de4b058ab4c0893970b8293d77d43
JakarandaJacklin <juja2568@colorado.edu>
2026-04-28 17:22:24 -0600

    CF2 Final commit

--- src/pytest.py
+++ src/pytest.py
@@ -9,7 +9,7 @@ sys.setrecursionlimit(20000)
 simulator.changeQuads([0,0], 800)
 bodies = simulator.initBodies(200, 1, 15, 0, 800) #mass 1-15
 simulator.changeG(0.01)#0.01)
-simulator.changeMerge(False, 0.2)#0.2)
+simulator.changeMerge(False, 0.4)#0.2)
 
 
 
@@ -30,7 +30,7 @@ def drawQuads(tree, screen):
     size = tree.size
 
     pygame.draw.rect(screen, (255, 131, 255), (x, y, size, size), width=1, border_radius=0)
-    pygame.draw.circle(screen, (235, 52, 116), tree.COM, 2)
+    pygame.draw.circle(screen, (235, 52, 116), tree.COM, np.sqrt(tree.mass))
 
 
     if tree.Children != []:
@@ -50,7 +50,7 @@ while running:
     screen.fill((0, 0, 0)) # Fill the screen with black
     for i in bodies:
         pygame.draw.circle(screen, (255, 255, 255), i.position, np.sqrt(i.mass))
-        drawQuads(tree, screen)
+        #drawQuads(tree, screen)
     # Draw other game elements here...
 
     # 4. Update the display to show the new frame
